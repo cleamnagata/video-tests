786b67306456cfd3347a5ae32319e112219e9f00
cleamnagata <nagata@cleamfield.jp>
2017-04-03 16:31:09 +0900

    test

--- src/init.js
+++ src/init.js
@@ -2,5 +2,6 @@ export default function(sceneClass, sceneArgs, manifests) {
   if (window.videoTestScene) {
     window.videoTestScene.close();
   }
+  console.log("aa");
   window.videoTestScene = new sceneClass(sceneArgs);
 }
